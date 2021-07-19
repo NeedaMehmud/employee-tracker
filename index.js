2779c65dd207906c0eb110b4a7ee991a05d8441c
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-19 14:31:30 -0400

    created viewDepartment function and succesfully returned data

--- index.js
+++ index.js
@@ -11,33 +11,65 @@ const connection = mysql.createConnection({
     user: 'root',
 
     // Be sure to update with your own MySQL password!
-    // password: '0985',
     database: 'employee_managementDB',
 });
 
-
+//'INSERT INTO products SET? iceCreamCRUD.js
 // Connect to the DB
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    //   createProduct();
     start();
 });
 
 function direction() {
     inquirer.prompt({
-        name: "direction",
-        type: "list",
-        message: "Welcome to Employee Management! What would you like to do?",
-        choice: ["View Department", "View Roles", " VIew Employees", "Add Department", "Add Roles", "Add Employees", "Update Employee's Role"]
+        name: 'direction',
+        type: 'list',
+        message: 'Welcome to Employee Management! What would you like to do?',
+        choices: [
+            'View Department',
+            'View Roles',
+            'View Employees',
+            'Add Department',
+            'Add Roles',
+            'Add Employees',
+            "Update Employee's Role",
+            'Exit']
 
-    // }).then((response) => {
+    })
+        .then((response) => {
+            if (response === "View Department") {
+                console.log("You chose View Department.../n");
+                viewDepartment();
+            } else if (response.direction === "Add Employees") {
+                console.log("You chose to Add Employees...\n");
+                AddEmployees();
+            }
+            else if (response.direction === "Exit") {
+                console.log("You chose to Exit .../n");
+                connection.end();
+            }
+        })
+}
 
+function AddEmployees(){
+    connection.query(
+
+    )
+}
+
+function viewDepartment() {
+    console.log("View Department function initialized");
+    connection.query("SELECT * from department", (err, data) => {
+        if (err) throw err;
+        console.table(data);
+        direction();
     })
 }
 
 function start() {
-    connection.query("SELECT * FROM employee", (err, data) => {
+    connection.query('SELECT * FROM employee', (err, data) => {
         if (err) throw err;
         console.table(data)
         direction();
