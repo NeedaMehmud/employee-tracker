8cb88d965018d688141b313b97da3b7b1c5c19dc
Needa Mehmud <needa.mehmud@gmail.com>
2021-07-29 13:33:26 -0400

    work in progress

--- Query.js
+++ Query.js
@@ -7,6 +7,35 @@ const connection = mysql.createConnection({
     database: 'employee_managementDB',
 });
 
+
+function selectAllRoles() {
+    let role = [];
+    connection.query("SELECT * FROM role",
+        (err, data) => {
+            if (err) throw err;
+            for (let i = 0; i < data.length; i++) {
+                role.push(data[i].title);
+            }
+        });
+    return role;
+}
+
+let manager = [];
+function selectManager() {
+    connection.query(
+        `SELECT first_name, 
+        last_name FROM employee 
+        WHERE manager_id IS NULL`,
+        (err, data) => {
+            if (err) throw err
+            for (let i = 0; i < data.length; i++) {
+                manager.push(data[i].first_name);
+            }
+        });
+    return manager;
+}
+
+
 class Queries {
     
     viewDepartment() {
@@ -57,31 +86,47 @@ class Queries {
             });
     }
 
-    selectAllRoles() {
-        let role = [];
-        connection.query("SELECT * FROM role",
-            (err, data) => {
-                if (err) throw err;
-                for (let i = 0; i < data.length; i++) {
-                    role.push(data[i].title);
-                }
-            });
-        return role;
-    }
-
-    selectManager() {
-        let manager = [];
-        connection.query(
-            `SELECT first_name, 
-        last_name FROM employee 
-        WHERE manager_id IS NULL`,
-            (err, data) => {
-                if (err) throw err
-                for (let i = 0; i < data.length; i++) {
-                    manager.push(data[i].first_name);
-                }
-            });
-        return manager;
+    addEmployee() { 
+        inquirer.prompt([
+            {
+              name: "firstname",
+              type: "input",
+              message: "Enter their first name "
+            },
+            {
+              name: "lastname",
+              type: "input",
+              message: "Enter their last name "
+            },
+            {
+              name: "role",
+              type: "list",
+              message: "What is their role? ",
+              choices: selectRole()
+            },
+            {
+                name: "choice",
+                type: "rawlist",
+                message: "Whats their managers name?",
+                choices: selectManager()
+            }
+        ]).then(function (val) {
+          var roleId = selectRole().indexOf(val.role) + 1
+          var managerId = selectManager().indexOf(val.choice) + 1
+          connection.query("INSERT INTO employee SET ?", 
+          {
+              first_name: val.firstName,
+              last_name: val.lastName,
+              manager_id: managerId,
+              role_id: roleId
+              
+          }, function(err){
+              if (err) throw err
+              console.table(val)
+              startPrompt()
+          })
+    
+      })
     }
 }
 
--- questions.js
+++ questions.js
@@ -12,7 +12,6 @@ const connection = mysql.createConnection({
     database: 'employee_managementDB',
 });
 
-
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
@@ -56,6 +55,7 @@ function direction() {
                 case "Add Department":
                     break;
                 case "Add Roles":
+                    query.addRole()
                     break;
                 case "Add Employees":
                     query.addEmployees()
@@ -72,174 +72,7 @@ function direction() {
         });
 }
 
-// function viewAllEmployees() {
-//     console.log("inside view all employees");
-//     connection.query(
-//         `SELECT employee.first_name, 
-//     employee.last_name, 
-//     role.title, role.salary, 
-//     department.name, CONCAT(e.first_name, ' ' ,e.last_name) 
-//     AS Manager FROM employee 
-//     INNER JOIN role on role.id = employee.role_id 
-//     INNER JOIN department on department.id = role.department_id 
-//     left join employee e on employee.manager_id = e.id;`,
-//         (err, data) => {
-//             if (err) throw err
-//             console.table(data)
-//         });
-// }
 
-// function viewDepartment() {
-//     console.log("View Department function initialized");
-//     connection.query(
-//         `SELECT employee.first_name, 
-//     employee.last_name, department.name 
-//     AS Department FROM employee 
-//     JOIN role ON employee.role_id = role.id 
-//     JOIN department ON role.department_id = department.id 
-//     ORDER BY employee.id;`,
-//         (err, data) => {
-//             if (err) throw err;
-//             console.table(data);
-//         });
-// }
-
-// function viewRoles() {
-//     connection.query(
-//         `SELECT employee.first_name, 
-//     employee.last_name, 
-//     role.title 
-//     AS Title FROM employee 
-//     JOIN role ON employee.role_id = role.id;`,
-//         (err, data) => {
-//             if (err) throw err;
-//             console.table(data);
-//         });
-// }
-
-// function selectAllRoles() {
-//     let role = [];
-//     connection.query("SELECT * FROM role",
-//         (err, data) => {
-//             if (err) throw err;
-//             for (let i = 0; i < data.length; i++) {
-//                 role.push(data[i].title);
-//             }
-//         });
-//     return role;
-// }
-
-// let manager = [];
-// function selectManager() {
-
-//     connection.query(
-//         `SELECT first_name, 
-//     last_name FROM employee 
-//     WHERE manager_id IS NULL`,
-//         (err, data) => {
-//             if (err) throw err
-//             for (let i = 0; i < data.length; i++) {
-//                 manager.push(data[i].first_name);
-//             }
-//         });
-//     return manager;
-// }
-
-function addEmployees() {
-    inquirer.prompt([
-        {
-            name: "firstname",
-            type: "input",
-            message: "Enter their first name "
-        },
-        {
-            name: "lastname",
-            type: "input",
-            message: "Enter their last name "
-        },
-        {
-            name: "role",
-            type: "list",
-            message: "What is their role? ",
-            choices: selectAllRoles()
-        },
-        {
-            name: "manager",
-            type: "list",
-            message: "Whats their managers name?",
-            choices: selectManager()
-        },
-    ]).then(function (data) {
-
-        console.log("manager: " + data.manager);
-
-        console.log(selectAllRoles());
-
-        let roleId = selectAllRoles().indexOf(data.role) + 1
-        let managerId = selectManager().indexOf(data.manager) + 1
-
-        console.log(roleId);
-        console.log(managerId);
-
-        connection.query(`INSERT INTO employee SET ?`,
-            {
-                first_name: data.firstName,
-                last_name: data.lastName,
-                manager_id: managerId,
-                role_id: roleId
-
-            }, (err) => {
-                if (err) throw err
-                console.table(data)
-                direction()
-            })
-    })
-}
-
-function updateEmployee() {
-    connection.query(
-        `SELECT employee.last_name, role.title 
-        FROM employee 
-        JOIN role ON employee.role_id = role.id;`,
-        (err, data) => {
-            if (err) throw err;
-            console.table(data);
-            inquirer.prompt([
-                {
-                    name: "lastName",
-                    type: "list",
-                    choices: () => {
-                        let employeelastName = [];
-                        for (let i = 0; i < data.length; i++) {
-                            employeelastName.push(data[i].last_name);
-                        }
-                        return employeelastName;
-                    },
-                    message: "What is the employee's last name? ",
-                },
-                {
-                    name: "role",
-                    type: "list",
-                    message: "What is employee's new title? ",
-                    choices: selectAllRoles()
-                },
-            ]).then((data) => {
-                let roleId = selectAllRoles().indexOf(data.role) + 1;
-                connection.query(`UPDATE employee SET WHERE ?`,
-                    {
-                        last_name: data.lastName
-                    },
-                    {
-                        role_id: roleId
-                    },
-                    (err, data) => {
-                        if (err) throw err
-                        console.table(data)
-                        direction()
-                    })
-            });
-        });
-}
 
 function start() {
     connection.query(`SELECT * FROM employee`, (err, data) => {
@@ -248,4 +81,10 @@ function start() {
         direction();
     })
 }
+
+
+
+    
+
+
 
